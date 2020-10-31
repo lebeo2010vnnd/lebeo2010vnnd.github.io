73410779e33c2bb40fbe446378962dcb4635a5b7
lebeo2010vnnd <leducanh2010vnnd@gmail.com>
2020-10-31 09:38:47 +0700

    js

--- js2/script.js
+++ js2/script.js
@@ -4,8 +4,7 @@ function minMax(numbers){
     return `Số lớn nhất là ${maxNumber} và số nhỏ nhất là ${minNumber}`;
 }
 
-function Bai3(xxx)
-            {
+function Bai3(xxx){
                 let soChan;
                 let soLe;
                 soChan = xxx.filter(number => (number % 2) == 0 );
@@ -13,4 +12,13 @@ function Bai3(xxx)
                 // return soLe[0];
                 // return soChan[soLe.length-1];
                 return ("Số Lẻ đầu tiên là " + soLe[0] + " và số Chẵn cuối cùng là " + soChan[soLe.length-1]);
-            }
+}
+
+
+ function thaysonguyen(arrs){
+     for(let i=0; i< arrs.length; i++){
+         if(arrs[i] < 0)
+            arrs[i] = 0
+     }
+     return `${arrs}`
+ }           
